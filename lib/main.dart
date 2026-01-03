d6d1924c8d8b30e0837e589f35eaf39c8a7940ab
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2026-01-03 23:19:11 +0700

    app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartGoalApp());
+}
+
+class SmartGoalApp extends StatelessWidget {
+  const SmartGoalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Goal',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
